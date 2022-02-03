14089650893f9268fd1ab240dce190d62ee1fc76
Agrit Tiwari <agrit1408@gmail.com>
2022-02-03 17:42:18 +0530

    figuring out authentication and mongodb integration

---
+++ pages/api/auth/example.js
@@ -0,0 +1,8 @@
+import { getProviders } from "next-auth/react"
+
+export default async (req, res) => {
+  const providers = await getProviders()
+  // console.log("Providers", providers)
+  res.json(providers)
+  res.end()
+}
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { useContext, useEffect } from 'react'
 
 import { SaveUserContext } from '../Contexts/userContext/userContext'
 
-import { connectToDatabase } from '../lib/mongodb'
+import clientPromise from '../lib/mongodb'
 import Dashboard from '../Components/Dashboard'
 import HomePage from '../Components/HomePage'
 
@@ -69,14 +69,20 @@ const Home = ({ isConnected }) =>
 export default Home;
 
 export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase()
-
-  const isConnected = await client.isConnected()
-
-  return {
-    props: {
-      isConnected
-      },
-    
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    await clientPromise
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
   }
 }
