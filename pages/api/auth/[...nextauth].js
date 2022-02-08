4afe391d28845fba1f13f3d1e13dfeba1527e8fc
Agrit Tiwari <agrit1408@gmail.com>
2022-02-08 15:11:40 +0530

    integrating Oauth with mongoDB

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,7 +4,10 @@ import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
 
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from '../../../lib/mongodb'
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     // OAuth authentication providers...
     // AppleProvider({
@@ -16,13 +19,20 @@ export default NextAuth({
     //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     // Passwordless / email sign in
     EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: 'Share-Online <no-reply@example.com>'
-    }),
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    })
   ]
 })
