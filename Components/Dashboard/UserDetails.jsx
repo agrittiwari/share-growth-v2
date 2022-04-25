70b81adf390e18589e6f39d2bcde58e5fc2e58c4
Agrit Tiwari <agrit1408@gmail.com>
2022-04-25 16:19:08 +0530

    added next.config.js for images optimization

--- Components/Dashboard/UserDetails.jsx
+++ Components/Dashboard/UserDetails.jsx
@@ -47,27 +47,27 @@ export const UserDetails =() =>{
     
        if (data) {
          console.log(data)
-      return null;}
-      //  return (   
-      //   <div className="flex flex-row  m-2 ">
-      //   <Image
-      //   className=" rounded-full h-40 w-40  border-2 border-yellow-800 p-1 mr-8"
-      //     src={data.user.image}
-      //     alt="User-Image"
-      //     width={500}
-      //     height={500}
-      //   />
-      // <img src={data.user.image} className=" rounded-full h-40 w-40  border-2 border-yellow-800 p-1 mr-8" />
-      // <div className="ml-20 mr-20">
-      //   <h1>👋 {data.user.name}</h1>
-      //   <p><Link href={`https://www.twitter.com/${data.user.twitterHandle}`}>{data.user.twitterHandle}</Link></p>
-      //   <p>🏀🎓 {data.user.bioLink}  </p>
-      // <button>
-      // <Link  href="/editProfile">Edit Profile</Link></button>  
-      // </div>
+      //return null;}
+       return (   
+        <div className="flex flex-row  m-2 ">
+        <Image
+        className=" rounded-full h-40 w-40  border-2 border-yellow-800 p-1 mr-8"
+          src={data.user.image}
+          alt="User-Image"
+          width={500}
+          height={500}
+        />
+      <img src={data.user.image} className=" rounded-full h-40 w-40  border-2 border-yellow-800 p-1 mr-8" />
+      <div className="ml-20 mr-20">
+        <h1>👋 {data.user.name}</h1>
+        <p><Link href={`https://www.twitter.com/${data.user.twitterHandle}`}>{data.user.twitterHandle}</Link></p>
+        <p>🏀🎓 {data.user.bioLink}  </p>
+      <button>
+      <Link  href="/editProfile">Edit Profile</Link></button>  
+      </div>
       
       
-      // </div>)}
+      </div>)}
   }
   
   if (status==='unauthenticated'){
